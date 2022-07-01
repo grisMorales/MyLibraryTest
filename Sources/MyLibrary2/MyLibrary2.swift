69051cf4d04f817e9b8d886f8a9eeb772ddfb04b
gris <griselda.juarez@frogdesign.com>
2022-06-30 21:52:34 -0500

    testing buton

--- Sources/MyLibrary2/MyLibrary2.swift
+++ Sources/MyLibrary2/MyLibrary2.swift
@@ -1,6 +1,21 @@
+import SwiftUI
+
 public struct MyLibrary2 {
     public private(set) var text = "Hello, World!"
 
     public init() {
     }
+    
+    func primaryButton(){
+        if #available(macOS 10.15, *) {
+            Button(action: {
+                print("Suscríbete a SwiftBeta")
+            }, label: {
+                Text("Suscríbete")
+            })
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
 }
