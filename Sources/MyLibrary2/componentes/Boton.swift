c058789664cacb8ee30f37d00ac3d702628a39c3
gris <griselda.juarez@frogdesign.com>
2022-07-03 22:07:07 -0500

    ottos

--- Sources/MyLibrary2/componentes/Boton.swift
+++ Sources/MyLibrary2/componentes/Boton.swift
@@ -2,22 +2,27 @@ import SwiftUI
 @available(iOS 13.0, *)
 public struct PackageButton: ButtonStyle {
     
-    let buttonFont = Font.custom("Zilla Slab", size: 20).weight(.bold)
+    public init() {
+        let buttonFont = Font.custom("Zilla Slab", size: 20).weight(.bold)
+        
+        public func makeBody(configuration: Self.Configuration) -> some View {
+            configuration
+                .label
+                .font(buttonFont)
+                .multilineTextAlignment(.center)
+                .lineLimit(1)
+                .padding(.horizontal, 10)
+                .foregroundColor(.white)
+                .offset(y: -1)
+                .frame(height: 30)
+                .background(Color.black)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .scaleEffect(configuration.isPressed ? 0.9 : 1)
+                .opacity(configuration.isPressed ? 0.6 : 1)
+                .animation(.spring())
+        }
     
-    public func makeBody(configuration: Self.Configuration) -> some View {
-        configuration
-            .label
-            .font(buttonFont)
-            .multilineTextAlignment(.center)
-            .lineLimit(1)
-            .padding(.horizontal, 10)
-            .foregroundColor(.white)
-            .offset(y: -1)
-            .frame(height: 30)
-            .background(Color.black)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .scaleEffect(configuration.isPressed ? 0.9 : 1)
-            .opacity(configuration.isPressed ? 0.6 : 1)
-            .animation(.spring())
     }
+    
+
 }
---
+++ Sources/MyLibrary2/componentes/Text.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Griselda Juarez Morales on 03/07/22.
+//
+
+import SwiftUI
+
+public struct TextPackage: View {
+    var body: some View {
+        Text("paquetesss")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
