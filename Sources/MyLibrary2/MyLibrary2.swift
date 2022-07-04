8ed7cbd33ff2db2fc17925d469402f61305f365b
gris <griselda.juarez@frogdesign.com>
2022-07-03 21:24:53 -0500

    vamos dengues

--- Sources/MyLibrary2/MyLibrary2.swift
+++ Sources/MyLibrary2/MyLibrary2.swift
@@ -1,11 +1,35 @@
-
+import SwiftUI
 
 @available(iOS 13.0, *)
+
 public struct MyLibrary2 {
     public private(set) var text = "Hello, World!"
-    public private(set) var BotonTest = PackageButton()
-    @available(iOS 13.0, *)
+   
+   
     public init() {
         
     }
+    
+    public struct IntentoBoton: ButtonStyle {
+        
+        let buttonFont = Font.custom("Zilla Slab", size: 20).weight(.bold)
+        
+        public func makeBody(configuration: Self.Configuration) -> some View {
+            configuration
+                .label
+                .font(buttonFont)
+                .multilineTextAlignment(.center)
+                .lineLimit(1)
+                .padding(.horizontal, 10)
+                .foregroundColor(.white)
+                .offset(y: -1)
+                .frame(height: 30)
+                .background(Color.black)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .scaleEffect(configuration.isPressed ? 0.9 : 1)
+                .opacity(configuration.isPressed ? 0.6 : 1)
+                .animation(.spring())
+        }
+    }
+
 }
