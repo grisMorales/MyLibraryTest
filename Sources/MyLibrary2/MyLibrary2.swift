3c1ef78fe00ed20280849c3069a4371307ab0712
gris <griselda.juarez@frogdesign.com>
2022-07-03 21:21:00 -0500

    again

--- Sources/MyLibrary2/MyLibrary2.swift
+++ Sources/MyLibrary2/MyLibrary2.swift
@@ -1,10 +1,11 @@
 
 
+@available(iOS 13.0, *)
 public struct MyLibrary2 {
     public private(set) var text = "Hello, World!"
-
+    public private(set) var BotonTest = PackageButton()
     @available(iOS 13.0, *)
     public init() {
-        PackageButton()
+        
     }
 }
