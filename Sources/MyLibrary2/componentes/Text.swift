cfcd9ed20214f5ea76c8d0d1e442a201cc31bb75
gris <griselda.juarez@frogdesign.com>
2022-07-03 22:11:36 -0500

    acaa otro

--- Sources/MyLibrary2/componentes/Text.swift
+++ Sources/MyLibrary2/componentes/Text.swift
@@ -1,20 +1,11 @@
-//
-//  SwiftUIView.swift
-//  
-//
-//  Created by Griselda Juarez Morales on 03/07/22.
-//
-
 import SwiftUI
 
-public struct TextPackage: View {
-    var body: some View {
-        Text("paquetesss")
+@available(iOS 13.0, macOS 10.15, *)
+public struct ReusableButton: View {
+    public init() {
+        
     }
-}
-
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
+    public var body: some View {
+        Text("REUSABLE BUTTON!")
     }
 }
