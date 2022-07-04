3bc495708a80f7e600b9d0242859bbc5ddad7c57
gris <griselda.juarez@frogdesign.com>
2022-07-03 20:52:55 -0500

    Testinggg Botoneen

--- Sources/MyLibrary2/MyLibrary2.swift
+++ Sources/MyLibrary2/MyLibrary2.swift
@@ -4,19 +4,6 @@ public struct MyLibrary2 {
     public private(set) var text = "Hello, World!"
 
     public init() {
-        print("Class has been initialised")
+
     }
-    
-    public func doSomething(){
-        print("Yeah, it works")
-    }
-    
-    /*func primaryButton(){
-            Button(action: {
-                print("Suscríbete a SwiftBeta")
-            }, label: {
-                Text("Suscríbete")
-            })
-    }*/
-    
 }
---
+++ Sources/MyLibrary2/componentes/Boton.swift
@@ -0,0 +1,21 @@
+public struct PackageButton: ButtonStyle {
+    
+    let buttonFont = Font.custom("Zilla Slab", size: 20).weight(.bold)
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration
+            .label
+            .font(buttonFont)
+            .multilineTextAlignment(.center)
+            .lineLimit(1)
+            .padding(.horizontal, 10)
+            .foregroundColor(.white)
+            .offset(y: -1)
+            .frame(height: 30)
+            .background(Color.black)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .scaleEffect(configuration.isPressed ? 0.9 : 1)
+            .opacity(configuration.isPressed ? 0.6 : 1)
+            .animation(.spring())
+    }
+}
