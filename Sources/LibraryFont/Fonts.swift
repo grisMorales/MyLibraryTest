12ae56ccb49ab42b7cb6f2a97a8059d060c747b8
Griselda Morales <griselda.juarez@frogdesign.com>
2022-08-17 12:42:27 -0500

    configurando fuentes

---
+++ Sources/LibraryFont/Fonts.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+public enum Fonts: String, CaseIterable {
+    case bold = "DynaPuff_Bold"
+    case medium = "DynaPuff_Medium"
+    case regular = "DynaPuff_Regular"
+    case extraBold = "DynaPuff_SemiBold"
+}
+
+@available(iOS 13.0, *)
+public extension Font.TextStyle {
+    var size: CGFloat {
+        switch self {
+        case .largeTitle: return 60
+        case .title: return 48
+        case .title2: return 34
+        case .title3: return 24
+        case .headline, .body: return 18
+        case .subheadline, .callout: return 16
+        case .footnote: return 14
+        case .caption, .caption2: return 12
+        @unknown default:
+            return 8
+        }
+    }
+}
+
+@available(iOS 14.0, *)
+public extension Font {
+    private static func custom(_ font: Fonts, relativeTo style: Font.TextStyle) -> Font {
+        custom(font.rawValue, size: style.size, relativeTo: style)
+    }
+
+    static let dynaPuff = custom(.regular, relativeTo: .body)
+    static let dynaPuffLargeTitle = custom(.bold, relativeTo: .largeTitle)
+    static let dynaPuffLargeTitleBlack = custom(.extraBold, relativeTo: .largeTitle)
+    static let dynaPuffLargeTitleSemiBold = custom(.medium, relativeTo: .largeTitle)
+    static let dynaPuffTitle1 = custom(.bold, relativeTo: .title)
+    static let dynaPuffHeadline = custom(.medium, relativeTo: .headline)
+    static let dynaPuffSubHeadline = custom(.regular, relativeTo: .subheadline)
+    static let dynaPuffSubHeadlineSemiBold = custom(.medium, relativeTo: .subheadline)
+    static let dynaPuffFootnote = custom(.regular, relativeTo: .footnote)
+    static let dynaPuffFootnoteSemiBold = custom(.medium, relativeTo: .footnote)
+    
+}
--- Sources/LibraryFont/LibraryFont.swift
+++ Sources/LibraryFont/LibraryFont.swift
@@ -4,9 +4,22 @@ import SwiftUI
 public struct LibraryFont{
     
     public static func registerFonts() {
-        JetBrainsMono.allCases.forEach {
+        Fonts.allCases.forEach {
             registerFont(bundle: .module, fontName: $0.rawValue, fontExtension: "ttf")
         }
+    }
+    
+    fileprivate static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
+
+           guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
+                 let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
+                 let font = CGFont(fontDataProvider) else {
+                     fatalError("Couldn't create font from data")
+           }
+
+           var error: Unmanaged<CFError>?
+
+           CTFontManagerRegisterGraphicsFont(font, &error)
     }
     
     public init() {
