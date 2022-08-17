2054872c9acb0de4df127b577388ed431c36c501
Griselda Morales <griselda.juarez@frogdesign.com>
2022-08-17 13:48:16 -0500

    haciendo pruebas

--- Sources/LibraryFont/LibraryFont.swift
+++ Sources/LibraryFont/LibraryFont.swift
@@ -1,29 +1,36 @@
 import SwiftUI
 
-@available(iOS 13.0, macOS 10.15, *)
-public struct LibraryFont{
-    
-    public static func registerFonts() {
-        dynaPuffFamily.allCases.forEach {
-            print($0.rawValue)
-            registerFont(bundle: .module, fontName: $0.rawValue, fontExtension: "ttf")
+public func registerFonts() {
+        _ = UIFont.registerFont(bundle: .module, fontName: "DynaPuff-Bold", fontExtension: "ttf")
+        _ = UIFont.registerFont(bundle: .module, fontName: "DynaPuff-Medium", fontExtension: "ttf")
+    }
+
+    extension UIFont {
+        static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) -> Bool {
+
+            guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension) else {
+                fatalError("Couldn't find font \(fontName)")
+            }
+
+            guard let fontDataProvider = CGDataProvider(url: fontURL as CFURL) else {
+                fatalError("Couldn't load data from the font \(fontName)")
+            }
+
+            guard let font = CGFont(fontDataProvider) else {
+                fatalError("Couldn't create font from data")
+            }
+
+            var error: Unmanaged<CFError>?
+            let success = CTFontManagerRegisterGraphicsFont(font, &error)
+            guard success else {
+                print("Error registering font: \(fontName). Maybe it was already registered.")
+                return false
+            }
+
+            return true
         }
     }
-    
-    fileprivate static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
 
-           guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
-                 let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
-                 let font = CGFont(fontDataProvider) else {
-                     fatalError("Couldn't create font from data")
-           }
-
-           var error: Unmanaged<CFError>?
-
-           CTFontManagerRegisterGraphicsFont(font, &error)
+    struct CustomFonts {
+        var text = "Hello, World!"
     }
-    
-    public init() {
-        
-    }
-}
