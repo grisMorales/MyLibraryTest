86c1897d9e675b8a94283bf59436a7a282733d9b
Griselda Morales <griselda.juarez@frogdesign.com>
2022-09-14 17:23:58 -0500

    Pruebas colores hexadecimales

--- Sources/ReusableButton/DLS/ Color.swift
+++ Sources/ReusableButton/DLS/ Color.swift
@@ -17,7 +17,9 @@ struct BaseColor{
     let darkPrimary = Color("darkPrimary")
     let lightPrimary = Color("lightPrimary")
     let gray = Color("gray")
+    let grisTest = Color("#145B97")
 }
+
 @available(iOS 13.0, *)
 struct TokenColor {
     let baseColor = BaseColor()
@@ -40,7 +42,7 @@ struct TokenColor {
     
     init() {
         /// themePrimary
-        self.textTheme = baseColor.themePrimary
+        self.textTheme = baseColor.grisTest
         self.buttonTheme = baseColor.themePrimary
         self.backgroundTheme = baseColor.themePrimary
         
@@ -64,8 +66,28 @@ struct TokenColor {
         self.inactive = baseColor.gray
     }
 }
+
 @available(iOS 13.0, *)
 extension Color {
     static let Token = TokenColor()
+}
+
+@available(iOS 13.0, *)
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
 }
 
